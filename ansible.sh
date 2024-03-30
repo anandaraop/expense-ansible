df3d76e68d63be12a27db5eb4d3ca2fe65da7f4b
SVR Kalluru <raghuk.vit@gmail.com>
2024-03-30 06:15:02 +0530

    Testing secure variables

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,5 @@
 component=$1
 
-ansible-playbook -i $component-$env.rdevops6a.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
+env >/tmp/env
+
+#ansible-playbook -i $component-$env.rdevops6a.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
